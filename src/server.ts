6ba4f83dc8f0b46cbbf836403e5578f4d97230f0
Guilherme Rocha <devguilhermerocha@gmail.com>
2024-05-15 17:31:55 -0300

    feat: added new routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import fastify, { FastifyInstance } from "fastify";
 
-
+import {userRoutes} from "./routes/user.routes"
+import { listRoutes} from "./routes/list.routes"
 
 const app: FastifyInstance = fastify({ logger:true })
 
+
+app.register(userRoutes,{
+  prefix: "/users"
+})
+app.register(listRoutes,{
+  prefix: "/list"
+})
 
 app.listen({
   port: 3100,
