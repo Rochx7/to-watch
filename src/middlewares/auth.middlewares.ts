7a8f927e76bfb0f76e542f60228b482fe3c8c11a
Guilherme Rocha <devguilhermerocha@gmail.com>
2024-05-15 17:32:06 -0300

    feat: create simple auth

---
+++ src/middlewares/auth.middlewares.ts
@@ -0,0 +1,9 @@
+export function authMiddleware(req,reply){
+  const apiToken = req.headers["token"]
+
+  if(!apiToken){
+    reply.status(401).send({
+      message: "Token is required",
+    })
+  }
+}
