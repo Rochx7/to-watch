aeecd9c2a842329f30de2188eecca17c8708b961
Guilherme Rocha <devguilhermerocha@gmail.com>
2024-05-21 17:26:45 -0300

    refactor: change usecases to controller

--- src/routes/list.routes.ts
+++ src/routes/list.routes.ts
@@ -1,52 +1,46 @@
 import { FastifyInstance } from "fastify";
-import { UserCreate } from "../interfaces/users.interface";
-import { ListUseCase } from "../usecases/list.usecase";
+import { ListController } from "../controllers/list.controller";
 import { ListCreate } from "../interfaces/list.interface";
 import { authMiddleware } from "../middlewares/auth.middlewares";
 
-export async function listRoutes(fastify: FastifyInstance){
-  const listUseCase = new ListUseCase()
-  fastify.addHook("preHandler", authMiddleware)
-  fastify.get<{Params: {id:number}}>("/:id", async(req, reply)=>{
-    const { id } = req.params
-    try{
-      const data = await listUseCase.getList({idUser: Number(id)})
-
-      return reply.send(data)
-    }catch (error) {
-      reply.send(error)
-    }
-  })
-
-  fastify.put<{Body:{itemToAdd:string, idUser:number}}>("/", async(req, reply)=>{
-
-    const {idUser, itemToAdd} = req.body
-
-    try{
-      const data = {idUser,itemToAdd}
-      const response = await listUseCase.addNewInWatchList(data)
-
-      return reply.send(response)
-    }catch (error) {
-      reply.send(error)
-    }
-  })
-
-  fastify.post<{Body: ListCreate}>("/", async(req, reply) => {
-    const { token } = req.headers["token"]
+export async function listRoutes(fastify: FastifyInstance) {
+  const listController = new ListController();
+  fastify.addHook("preHandler", authMiddleware);
+  fastify.get<{ Params: { id: number } }>("/:id", async (req, reply) => {
+    const { id } = req.params;
     try {
-     const payload = {
-      userToken: token,
-      watchList: [],
-      liked: [],
-      watched: []
-     }
-      const data = await listUseCase.create({...payload})
+      const data = await listController.getList({ idUser: Number(id) });
 
-      return reply.send(data)
-      
+      return reply.send(data);
     } catch (error) {
-      reply.send(error)
+      reply.send(error);
     }
-  })
+  });
+
+  fastify.put<{ Body: { itemToAdd: string; idUser: number } }>(
+    "/",
+    async (req, reply) => {
+      const { idUser, itemToAdd } = req.body;
+
+      try {
+        const data = { idUser, itemToAdd };
+        const response = await listController.addNewInWatchList(data);
+
+        return reply.send(response);
+      } catch (error) {
+        reply.send(error);
+      }
+    }
+  );
+
+  fastify.post<{ Body: ListCreate }>("/", async (req, reply) => {
+    const { token } = req.headers["token"];
+    try {
+      const data = await listController.create(token);
+
+      return reply.send(data);
+    } catch (error) {
+      reply.send(error);
+    }
+  });
 }
