e00bf045089eec00635384a9b049c32952bf6a71
Guilherme Rocha <devguilhermerocha@gmail.com>
2024-06-03 20:06:25 -0300

    style: indentação

--- src/repositories/list.repository.ts
+++ src/repositories/list.repository.ts
@@ -1,57 +1,62 @@
 import { prisma } from "../database/prisma-client";
 import { List, ListRepository } from "../interfaces/list.interface";
 
-class ListRepositoryPrisma implements ListRepository{
-  async createList({ userToken }:{ userToken:number }): Promise<List> {   
+class ListRepositoryPrisma implements ListRepository {
+  async createList({ userToken }: { userToken: number }): Promise<List> {
     const result = await prisma.list.create({
-      data:{
+      data: {
         liked: [],
         watched: [],
         watchList: [],
-        userId: userToken
-      }
-    })
-
-    return result || null
-  }
-  async getListByIdUser({ idUser }:{ idUser:number }): Promise<List[]> {   
-    const result = await prisma.list.findMany({
-      where:{
-        user:{
-          id: idUser
-        }
+        userId: userToken,
       },
-      include:{
-        user:true
-      }
-    })
+    });
 
-    return result || null
+    return result || null;
+  }
+  async getListByIdUser({ idUser }: { idUser: number }): Promise<List[]> {
+    const result = await prisma.list.findMany({
+      where: {
+        user: {
+          id: idUser,
+        },
+      },
+      include: {
+        user: false,
+      },
+    });
+
+    return result || null;
   }
 
-  async addNewInWatchList({idUser,itemToAdd}:{idUser:number,itemToAdd:string}): Promise<List | null> {   
-    
+  async addNewInWatchList({
+    idUser,
+    itemToAdd,
+  }: {
+    idUser: number;
+    itemToAdd: string;
+  }): Promise<List | null> {
     try {
       const user = await prisma.user.findUnique({
         where: {
           id: idUser,
         },
       });
-  
+
       if (!user) {
-        throw new Error('User not found.');
+        throw new Error("User not found.");
       }
-  
+
       const list = await prisma.list.findFirst({
         where: {
           userId: idUser,
         },
       });
-  
+
       if (!list) {
-        throw new Error('List not found.');
+        throw new Error("List not found.");
       }
-  
+
       const updatedList = await prisma.list.update({
         where: {
           id: list.id,
@@ -62,13 +67,12 @@ class ListRepositoryPrisma implements ListRepository{
           },
         },
       });
-  
+
       return updatedList;
     } catch (error) {
-      console.error('Error adding item to watchList:', error);
+      console.error("Error adding item to watchList:", error);
       return null;
     }
   }
-  
 }
-export { ListRepositoryPrisma }
+export { ListRepositoryPrisma };
